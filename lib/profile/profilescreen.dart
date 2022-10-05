4c357e20307d6513b28ffd8cfaebed621dda0b4f
AbdulsalamAbdulrahman <asquare747@gmail.com>
2022-10-05 17:08:29 +0100

    email submission

--- lib/profile/profilescreen.dart
+++ lib/profile/profilescreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:http/http.dart' as http;
 
 class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
@@ -25,9 +28,19 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   final key = GlobalKey<FormState>();
 
+  String phpurl = 'https://kadunaelectric.com/meterreading/kecs/profile.php';
+
   String fullname = '';
   String payrollid = "";
   String phonenumber = '';
+  String id = '';
+  // String email = '';
+
+  bool _isLoading = false;
+  late bool error, sending, success;
+  late String msg;
+
+  TextEditingController email = TextEditingController();
 
   @override
   void initState() {
@@ -50,6 +63,48 @@ class _ProfileScreenState extends State<ProfileScreen> {
       fullname = pref.getString("fullname")!;
       payrollid = pref.getString("payrollid")!;
       phonenumber = pref.getString("phonenumber")!;
+    });
+  }
+
+  Future updateData() async {
+    setState(() {
+      _isLoading = true;
+    });
+    var res = await http.post(Uri.parse(phpurl), body: {
+      "id": id,
+      "email": email.text,
+    }); //sending post request with header data
+
+    if (res.statusCode == 200) {
+      debugPrint(res.body); //print raw response on console
+      var data = json.decode(res.body); //decoding json to array
+      if (data["error"]) {
+        setState(() {
+          //refresh the UI when error is recieved from server
+          sending = false;
+          error = true;
+          msg = data["message"]; //error message from server
+        });
+      } else {
+        showMessage('Data Submitted Succesfully');
+        //after write success, make fields empty
+
+        setState(() {
+          sending = false;
+          success = true; //mark success and refresh UI with setState
+        });
+      }
+    } else {
+      //there is error
+      setState(() {
+        error = true;
+        msg = "Error during sending data.";
+        sending = false;
+        //mark error and refresh UI with setState
+      });
+    }
+    setState(() {
+      _isLoading = false;
     });
   }
 
@@ -79,7 +134,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       const SizedBox(height: 10.0),
                       textField('Phone', phonenumber, true),
                       const SizedBox(height: 10.0),
-                      textField('Email', '', true),
+                      textFieldE('Email', true),
                       const SizedBox(height: 10.0),
                       textField('Payroll ID', payrollid, false),
                       const SizedBox(height: 10.0),
@@ -110,21 +165,75 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
-  Widget button(String text) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        minimumSize: const Size(500, 50),
-        maximumSize: const Size(500, 50),
+  Widget textFieldE(String text, _bool) {
+    BorderRadius.circular(30.0);
+    return Material(
+      child: TextFormField(
+        // initialValue: initial,
+        enabled: _bool,
+        controller: email,
+        keyboardType: TextInputType.text,
+        decoration: decorate(text),
+        validator: validateField,
       ),
-      child: Container(
-        alignment: Alignment.center,
-        child: Text(
-          text,
+    );
+  }
+
+  Widget button(String text) {
+    return ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(500, 50),
+          maximumSize: const Size(500, 50),
+        ),
+        icon: _isLoading
+            ? const SizedBox(
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ),
+                height: 15.0,
+                width: 15.0,
+              )
+            : const Text(''),
+        label: Text(
+          _isLoading ? '' : 'Submit',
           style: const TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
         ),
-      ),
-      onPressed: () {},
+        onPressed: () async {
+          if (key.currentState!.validate()) {
+            _isLoading ? null : updateData();
+          }
+        });
+  }
+
+  Future<dynamic> showMessage(String msg) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(msg),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).pop();
+                Navigator.of(context).pop();
+                // Navigator.of(context).pushAndRemoveUntil(
+                //     MaterialPageRoute(builder: (context) => const BillScreen()),
+                //     (route) => false);
+              },
+            ),
+          ],
+        );
+      },
     );
+  }
+
+  String? validateField(value) {
+    if (value.isEmpty) {
+      return "field is required";
+    }
+    return null;
   }
 
   InputDecoration decorate(String label) {
