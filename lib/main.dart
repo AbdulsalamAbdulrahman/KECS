6d373d6e5523c604178a68541c533da6d7f29964
AbdulsalamAbdulrahman <asquare747@gmail.com>
2022-11-07 16:18:15 +0100

    app title changed

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'KECS',
+      title: 'KE_Toolkit',
       theme: ThemeData(
         primarySwatch: Colors.green,
         textTheme: GoogleFonts.latoTextTheme(
