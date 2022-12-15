f9d4caaad3a17bed1bdb6c2da4c975d8362d9cf5
Abdulsalam Abdulrahman <asquare747@gmail.com>
2022-12-15 15:14:43 +0100

    commit

--- lib/bill/notdelivered.dart
+++ lib/bill/notdelivered.dart
@@ -163,12 +163,12 @@ class _NotDeliveredScreenState extends State<NotDeliveredScreen> {
                                 ),
                                 onPressed: () async {
                                   if (key.currentState!.validate()) {
-                                    if (widget.geolat == "" &&
-                                        widget.geolong == "") {
+                                    if (widget.geolat != "" &&
+                                        widget.geolong != "") {
+                                      _isLoading ? null : sendData();
+                                    } else {
                                       showMessageG(
                                           'Geolocation is needed. Make sure you have given the app location permission and your location is switched on');
-                                    } else {
-                                      _isLoading ? null : sendData();
                                     }
                                   }
                                 }),
